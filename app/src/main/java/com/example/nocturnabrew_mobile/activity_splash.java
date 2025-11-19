d0752eeecefc478ca8fa205c15896536c66edb72
laraalo02 <laraalo913@gmail.com>
2025-11-18 22:31:38 -0600

    Initial commit

---
+++ app/src/main/java/com/example/nocturnabrew_mobile/activity_splash.java
@@ -0,0 +1,13 @@
+package com.example.nocturnabrew_mobile;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class activity_splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
