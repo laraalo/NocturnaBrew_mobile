bca2c5a39a4e4c3baec633f42547bd6d3c6b3545
Emmanuel <st4004@utr.edu.mx>
2025-11-20 20:58:53 -0600

    creation of the first functional view

--- app/src/main/java/com/example/nocturnabrew_mobile/activity_splash.java
+++ app/src/main/java/com/example/nocturnabrew_mobile/activity_splash.java
@@ -1,13 +1,21 @@
 package com.example.nocturnabrew_mobile;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class activity_splash extends AppCompatActivity {
-
+    private static final int SPLASH_DURATION = 3000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(()->{
+            Intent intent = new Intent(activity_splash.this, WelcomeActivity.class );
+            startActivity(intent);
+            finish();
+        }, SPLASH_DURATION);
     }
 }
